345013b454554ce53c368dd38eaa3df02177929b
Orest Chornyi <orest.chorny@gmail.com>
2025-01-27 17:54:09 +0200

    Added vue-star-rating dependency and implement rating display in ReviewSection

--- src/components/Sections/ReviewSection.vue
+++ src/components/Sections/ReviewSection.vue
@@ -5,12 +5,33 @@
         <img src="@/img/google.svg" alt="Google logo" class="review-logo" />
         <h1 class="review-title">Відгуки наших клієнтів у Google</h1>
       </div>
+      <div class="review__rating">
+        <div class="review__rating-container">
+          <span class="review__rating-value">4.9</span>
+          <star-rating
+            :rating="4.5"
+            :read-only="true"
+            :star-size="10"
+            :max-rating="5"
+            :show-rating="false"
+            :round-start-rating="false"
+            :border-width="2"
+            border-color="#FCC141"
+            active-color="#FCC141"
+            inactive-color="#fff"
+            :gap="6"
+          />
+        </div>
+        <span class="review__review-count">123 відгуки</span>
+      </div>
     </div>
     <div class="review-buttons"></div>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StarRating from 'vue-star-rating'
+</script>
 
 <style lang="scss" scoped>
 @import '@/assets/main.scss';
@@ -47,5 +68,36 @@
   font-style: normal;
   font-weight: 500;
   line-height: 26px;
+}
+
+.review__rating {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 6px;
+}
+
+.review__rating-container {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.review__rating-value {
+  color: $main-text-color;
+  font-family: Greenwich;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 32px;
+}
+
+.review__review-count {
+  color: $secondary-text-color;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
 }
 </style>
