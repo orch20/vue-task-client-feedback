d23d542399bf8d80bd9483038721b8a93a2f5e7b
Orest Chornyi <orest.chorny@gmail.com>
2025-01-27 15:21:50 +0200

    Add ReviewSection component and main layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<script setup lang="ts"></script>
+<template>
+  <main class="main">
+    <ReviewSection />
+  </main>
+</template>
 
-<template></template>
+<script setup lang="ts">
+import ReviewSection from '@/components/Sections/ReviewSection.vue'
+</script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Sections/ReviewSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="review">
+    <div class="review-content">
+      <div class="review-header">
+        <img src="@/img/google.svg" alt="Google logo" class="review-logo" />
+        <h1 class="review-title">Відгуки наших клієнтів у Google</h1>
+      </div>
+    </div>
+    <div class="review-buttons"></div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+@import '@/assets/main.scss';
+
+.review {
+  width: 328px;
+  display: flex;
+  padding: 30px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 20px;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.05);
+}
+
+.review-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 20px;
+}
+
+.review-header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.review-title {
+  color: $main-text-color;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 26px;
+}
+</style>
